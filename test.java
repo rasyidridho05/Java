7b35042164666d498675a39ae4dbb4272eba2f32
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2024-10-30 11:36:46 +0700

    add: Faktorial

--- test.java
+++ test.java
@@ -1,8 +1,20 @@
+
 import java.util.Scanner;
 
 public class test {
 
     public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
+
+        System.out.println("Input N : ");
+        int n = Input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if ((n % i) == 0) {
+                System.out.println(i + " true");
+            } else {
+                System.out.println(i + " false");
+            }
+        }
     }
 }
